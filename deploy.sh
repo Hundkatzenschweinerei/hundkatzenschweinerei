6b5ed55306f00666c6589b65d8dd166ab1750450
Stefan Grüttner <s.gruettner@searchmetrics.com>
2017-12-15 12:39:10 +0100

    fix: sftp upload

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-jekyll build
-scp -rp -P 42724 _site hund-katze-schweinerei.de@88.198.51.48:/www
+jekyll build --destination www 
+sftp -P 42724 hund-katze-schweinerei.de@88.198.51.48:/ <<< $'put -r www'
 
