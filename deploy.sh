41e03b9cd6958311b516fb9a7b2c57764e15e9de
Stefan Grüttner <s.gruettner@searchmetrics.com>
2017-12-15 13:00:09 +0100

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-jekyll build --destination www 
-sftp -P 42724 hund-katze-schweinerei.de@88.198.51.48:/ <<< $'put -r www'
+jekyll build --destination www
+echo 'put -r www' | sftp -P 42724 hund-katze-schweinerei.de@88.198.51.48:/
 
